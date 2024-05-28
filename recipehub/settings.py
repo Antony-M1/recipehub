9c9162db6268425f465d80a34a69ff01d2e3a2c5
Antony-M1 <antony.praveenkumar@softsuave.com>
2024-05-28 18:41:38 +0530

    First API Created Initialise

--- recipehub/settings.py
+++ recipehub/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes'
+    'recipes',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- recipes/urls.py
+++ recipes/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('recipes.urls')),
+    path('home', home, name='home'),
 ]
